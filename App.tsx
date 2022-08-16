e13db88c01f08922cddb2f730878320610ba9746
danielleond99 <danielleond99@gmail.com>
2022-08-16 17:36:35 -0400

    init commit

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Text} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Text>AppScreen</Text>
+    </NavigationContainer>
+  );
+};
+
+export default App;
